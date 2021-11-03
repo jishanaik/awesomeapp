96c240489e08e13150d73dc70d38672d0d5124f5
Jisha Naik <jishanaik@gmail.com>
2021-11-04 00:08:19 +0530

    ch2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -15,9 +15,37 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Awesome App"),
       ),
-      body: Container(
-        child: Center(child: Text("Hi Flutter!")),
-      )
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(8),
+          alignment: Alignment.center,
+          width: 100,
+          height: 100,
+          decoration: BoxDecoration(
+            shape: BoxShape.rectangle,
+            borderRadius: BorderRadius.circular(10),
+            boxShadow: [BoxShadow(
+              color: Colors.grey.shade400,
+              blurRadius: 5,
+              offset: Offset(2.0,5.0))
+            ],
+            color: Colors.teal,
+          gradient: LinearGradient(colors: [
+            Colors.red,
+            Colors.pink,
+          ]),
+          ),
+          child: Text(
+            "I am a box",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.bold,
+              fontSize: 20
+            ),
+            ),
+        ),
+      )    
     );
   }
 }
