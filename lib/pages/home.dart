b9df68da495467b112e5450289e26b97be5a8c09
Jisha Naik <jishanaik@gmail.com>
2021-11-11 20:32:10 +0530

    ch6 completed

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,7 +1,14 @@
+// ignore_for_file: avoid_print
+
+import 'dart:convert';
 import 'dart:ffi';
+import 'dart:html';
+import 'dart:async';
+import 'package:http/src/response.dart';
 import 'package:awesomeapp/bg_image.dart';
 import 'package:awesomeapp/drawer.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class HomePage extends StatefulWidget {
 
@@ -10,13 +17,23 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  TextEditingController _nameController= TextEditingController();
-  var myText="Change me";
+  //TextEditingController _nameController= TextEditingController();
+  //var myText="Change me";
+  
+  var url= "https://jsonplaceholder.typicode.com/photos";
+  
+
   @override
   void initState() {
     super.initState();
+    fetchData();
+  }
+
+  fetchData()async{
+    var res = http.get(Uri.https('https://jsonplaceholder.typicode.com/photos', 'api/people'));
     
   }
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -24,41 +41,12 @@ class _HomePageState extends State<HomePage> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: SingleChildScrollView(
-          child: Card(
-            child:  Column(
-              children: [
-                BgImage(),
-                Text(
-                  myText,
-                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: TextField(
-                    controller: _nameController,
-                    keyboardType: TextInputType.text,
-                    obscureText: false,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      hintText: "Enter something here",
-                      labelText: "Name"
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
       ),
       drawer: MyDrawer(),
         floatingActionButton: FloatingActionButton(
         onPressed: () {
-          myText=_nameController.text;
-          setState(() {});
+          //myText=_nameController.text;
+          //setState(() {});
         },
         child: Icon(Icons.refresh),
         ),  
